f1bebe89aaa1ef270a08c9ec2219d6af6072d181
Lucas <lfpgc96@hotmail.com>
2018-11-15 17:03:24 -0300

    Adicionado estrutura para medição

--- algoritmos_ordenacao.c
+++ algoritmos_ordenacao.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <linux/perf_event.h>
+#include <asm/unistd.h>
 
 /*Procedimento para criar e inicializar um array de tamanho t
 *e retorna-lo. O array sera inicializado com valores de tras pra frente
@@ -13,13 +18,65 @@ int *inicializa_array(int t){
     return array;
 }
 
+long perf_event_open(struct perf_event_attr *hw_event, pid_t pid, int cpu, int group_fd, unsigned long flags){
+	int ret;
+	ret = syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
+	return ret;
+}
 
+struct perf_event_attr* startPerfStruct(int type, int *fd){
+	struct perf_event_attr *aux = calloc(1,sizeof(struct perf_event_attr));
+	memset(aux, 0, sizeof(struct perf_event_attr));
+	switch(type){
+		case 1: //L1 CACHE
+			aux->type = PERF_TYPE_HW_CACHE;
+			aux->size = sizeof(struct perf_event_attr);
+			aux->config = (PERF_COUNT_HW_CACHE_L1D) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
+			aux->disabled = 1;
+			aux->exclude_kernel = 1;
+			aux->exclude_hv = 1;
+			aux->exclude_idle = 1;
+			break;
+		case 2: //LL CACHE
+			aux->type = PERF_TYPE_HW_CACHE;
+			aux->size = sizeof(struct perf_event_attr);
+			aux->config = (PERF_COUNT_HW_CACHE_LL) | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
+			aux->disabled = 1;
+			aux->exclude_kernel = 1;
+			aux->exclude_hv = 1;
+			aux->exclude_idle=1;
+			break;
+		default:
+			return 0;
+	}
+	*fd = perf_event_open(aux, 0, -1, -1, 0);
+	if(*fd == -1){
+		fprintf(stderr, "Error opening leader %llx\n", aux->config);
+		exit(EXIT_FAILURE);
+	}
+	ioctl(*fd, PERF_EVENT_IOC_RESET, 0);
+	return aux;
+}
+
+
+void ativarPerf(int fd){
+	ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+}
+
+void desativarPerf(int fd){
+	ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+}
 
 int main(){
+    int fd_cacheL1, fd_cacheLL;
+    struct perf_event_attr* cacheL1 = startPerfStruct(1, &fd_cacheL1);
+    struct perf_event_attr* cacheLL = startPerfStruct(1, &fd_cacheLL);
+    ativarPerf(fd_cacheL1);
+    ativarPerf(fd_cacheLL);
     int isFinishedRunning = 0;
     int *array = NULL;
     while(!isFinishedRunning){
-        int opc = 0;        
+        int opc = 0;
         printf("Selecione o algoritmo de ordenacao:\n");
         printf("1->QuickSort\n");
         printf("2->RadixSort\n");
@@ -68,6 +125,15 @@ int main(){
             break;            
         }
     }
-
+    desativarPerf(fd_cacheL1);
+    desativarPerf(fd_cacheLL);
+    long long count;
+    read(fd_cacheL1, &count, sizeof(long long));
+    printf("Used %lld cache L1D misses\n", count);
+    close (fd_cacheL1);
+    read(fd_cacheLL, &count, sizeof(long long));
+    printf("Used %lld cache LL Misses\n", count);
+    close(fd_cacheLL);
     return 0;
 }
+
