80b6f4a5fb4c18969330dc8a04b82b404a954d8b
IsmaelCesar <leamscesar@gmail.com>
2018-11-19 15:00:58 -0300

    Utilizando perf stat

--- perfstat.sh
+++ perfstat.sh
@@ -3,9 +3,10 @@
 pidmain=`pidof main`
 echo  'PID:' $pidmain
 
-perfevent=`perf list |grep cache-misses | awk '{print $1}'|sed -n '1p'`
-
+#eventos a serem monitorados
+cacheMissesEv=`perf list |grep cache-misses | awk '{print $1}'|sed -n '1p'`
+L1dCacheLoads=`perf list | grep L1-dcache-loads|awk '{print $1}'`
 #echo  'Evento monitorado:'$perfevent
-out=`perf stat -B -e $perfevent -p $pidmain`
+out=`perf stat -B -e $cacheMissesEv -p $pidmain`
 
 #echo $out
