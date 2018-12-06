c8d09d46e0a586b07f8fc54d149ceae109f8d67b
leamscesar@gmail.com <leamscesar@gmail.com>
2018-12-06 11:32:41 -0300

    Passando tamanho do array via terminal

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,5 +1,13 @@
+
 #!/bin/bash
 #Rodar script em modo administrador
-gcc algoritmos_ordenacao.c -o main
+declare -a powers=("10" "32" "100" "317" "1000" "3163" "10000" "31623" "100000" "316228" "1000000" "3162278" "10000000" "31622777" "100000000" "316227766")
 
-./main > "tempo"/cache_merge_t100000000.txt
+algoritmo="counting"
+metodo="ordenacao"
+
+gcc algoritmos_ordenacao_tempo.c -o main
+
+./main ${powers[0]} > "tempo_ismael"/$metodo_$algoritmo_${powers[0]}.txt
+
+echo "FINISHED!"
