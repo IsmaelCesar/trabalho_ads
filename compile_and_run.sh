4b67609437f15d9ad5bdb678511f532b1bed5af9
leamscesar@gmail.com <leamscesar@gmail.com>
2018-12-06 11:39:13 -0300

    Adicionando pasta

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -4,6 +4,8 @@
 #potencias
 declare -a powers=("10" "32" "100" "317" "1000" "3163" "10000" "31623" "100000" "316228" "1000000" "3162278" "10000000" "31622777" "100000000" "316227766")
 
+pasta="tempo_ismael"
+
 metodo="ordenacao"
 algoritmo="counting"
 
@@ -12,6 +14,6 @@ index=0                                  #indice para controle do array
 while[index -lt 4]
 do
     echo "Iteration $index"
-    ./main ${powers[$index]} > "tempo_ismael"/$metodo_$algoritmo_${powers[$index]}.txt
+    ./main ${powers[$index]} > $pasta/$metodo_$algoritmo_${powers[$index]}.txt
 done
 echo "FINISHED!"
