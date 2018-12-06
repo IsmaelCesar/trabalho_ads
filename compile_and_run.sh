3c38cb4b0fbf5c7bf336fc76ed79ea1225434bc0
leamscesar@gmail.com <leamscesar@gmail.com>
2018-12-06 11:38:12 -0300

    Adicionando loop

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,13 +1,17 @@
 
 #!/bin/bash
 #Rodar script em modo administrador
+#potencias
 declare -a powers=("10" "32" "100" "317" "1000" "3163" "10000" "31623" "100000" "316228" "1000000" "3162278" "10000000" "31622777" "100000000" "316227766")
 
-algoritmo="counting"
 metodo="ordenacao"
+algoritmo="counting"
 
-gcc algoritmos_ordenacao_tempo.c -o main
-
-./main ${powers[0]} > "tempo_ismael"/$metodo_$algoritmo_${powers[0]}.txt
-
+gcc algoritmos_ordenacao_tempo.c -o main #Compilando implementacao
+index=0                                  #indice para controle do array
+while[index -lt 4]
+do
+    echo "Iteration $index"
+    ./main ${powers[$index]} > "tempo_ismael"/$metodo_$algoritmo_${powers[$index]}.txt
+done
 echo "FINISHED!"
