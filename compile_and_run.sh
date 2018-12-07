d07d3be4d44a669b9bc10a3cc77e8276a0faa5df
leamscesar@gmail.com <leamscesar@gmail.com>
2018-12-07 18:07:16 -0300

    Pequenas alteracoes no compile and Ru

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -3,7 +3,7 @@
 #potencias
 declare -a powers=("10" "32" "100" "317" "1000" "3163" "10000" "31623" "100000" "316228" "1000000" "3162278" "10000000" "31622777" "100000000" "316227766")
 
-metodo="ordenacao"
+metodo="cache"
 declare -a algoritmo=("quick" "radix" "heap" "merge" "counting" "insertion" "bubble")
 
 gcc algoritmos_ordenacao_tempo.c -o main #Compilando implementacao
